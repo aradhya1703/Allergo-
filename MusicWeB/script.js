ed977fe77e72e410887e3af19aa5cf059c60134e
Aradhya Kumar Singh <115171371+aradhya1703@users.noreply.github.com>
2024-01-03 12:40:18 +0530

    Add files via upload

---
+++ MusicWeB/script.js
@@ -0,0 +1,116 @@
+console.log("Welcome to Allergomusic");
+
+// making  the Variables-
+let songIndex = 0;
+let audioElement = new Audio('songs/1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
+    {songName: "Allergo Special Mix", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Coolio-Gangster's Paradise", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "Daku", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Divine-3.59", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Tera Yarr Hoon Mai", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Kaise Hua-Kabir Singh", filePath: "songs/6.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Way Down Go-Kaleo", filePath: "songs/7.mp3", coverPath: "covers/7.jpg"},
+    {songName: "These Days-SMW", filePath: "songs/8.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Afterhours-TeraKiyoBoyzz", filePath: "songs/9.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Love Me Thoda-Yariyaan", filePath: "songs/10.mp3", coverPath: "covers/10.jpg"},
+    {songName: "Mann Bharya 2.0", filePath: "songs/11.mp3", coverPath: "covers/11.jpg"},
+    // {songName: "Love Me Thoda-Yariyaan", filePath: "songs/10.mp3", coverPath: "covers/10.jpg"},
+]
+
+songItems.forEach((element, i)=>{ 
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+})
+ 
+
+// Handle play/pause click action to play music-
+
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+
+// Listen to Events-
+
+audioElement.addEventListener('timeupdate', ()=>{ 
+
+    // Update the loadbar music duration-
+
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{ 
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=9){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
